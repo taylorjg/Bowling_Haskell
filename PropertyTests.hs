64daa64482bb4b6e68f2b595b56d312104bb8d9e
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-08-08 10:59:51 +0100

    Simplification of the rollsFromRollsFor12Frames function

--- PropertyTests.hs
+++ PropertyTests.hs
@@ -4,20 +4,18 @@ import System.Random
 import Bowling
 import Control.Monad (join)
 
-rollsFromRollsFor12rames :: [Rolls] -> Rolls
-rollsFromRollsFor12rames rollsFor12Frames =
+rollsFromRollsFor12Frames :: [Rolls] -> Rolls
+rollsFromRollsFor12Frames rollsFor12Frames =
     rollsFor10Frames ++ take numLastFrameBonusBalls remainingRolls
     where
         rollsFor10Frames = join $ take 10 rollsFor12Frames
         remainingRolls = join $ drop 10 rollsFor12Frames
-        f10 = rollsFor12Frames !! 9
-        f10r1 = head f10
-        f10sum = sum f10
-        numLastFrameBonusBalls = if f10r1 == maxPins
-            then 2
-            else if f10sum == maxPins
-                then 1
-                else 0
+        lastFrame = rollsFor12Frames !! 9
+        numLastFrameBonusBalls = calculateNumLastFrameBonusBalls lastFrame
+        calculateNumLastFrameBonusBalls [maxPins] = 2
+        calculateNumLastFrameBonusBalls [r1, r2]
+            | r1 + r2 == maxPins = 1
+            | otherwise = 0
 
 prop_FrameIntegrityAfterOneRoll :: [Rolls] -> Bool
 prop_FrameIntegrityAfterOneRoll rollsFor12Frames =
@@ -26,7 +24,7 @@ prop_FrameIntegrityAfterOneRoll rollsFor12Frames =
     secondRoll f1 == Nothing &&
     thirdRoll f1 == Nothing
     where
-        rs = rollsFromRollsFor12rames rollsFor12Frames
+        rs = rollsFromRollsFor12Frames rollsFor12Frames
         r1 = rs !! 0
         frames = processRolls $ take 1 rs
         f1 = frames !! 0
@@ -44,7 +42,7 @@ prop_FrameIntegrityAfterTwoRolls rollsFor12Frames =
         secondRoll f1 == Just r2 &&
         thirdRoll f1 == Nothing
     where
-        rs = rollsFromRollsFor12rames rollsFor12Frames
+        rs = rollsFromRollsFor12Frames rollsFor12Frames
         r1 = rs !! 0
         r2 = rs !! 1
         frames = processRolls $ take 2 rs
