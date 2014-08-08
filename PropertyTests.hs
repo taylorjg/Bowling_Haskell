22c164f0919b4c8ad7312ac5b3aa3ebab948cf35
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-08-08 09:53:38 +0100

    Extracted a function to build a valid list of rolls from rolls for 12 random frames

--- PropertyTests.hs
+++ PropertyTests.hs
@@ -4,20 +4,35 @@ import System.Random
 import Bowling
 import Control.Monad (join)
 
+rollsFromRollsFor12rames :: [Rolls] -> Rolls
+rollsFromRollsFor12rames rollsFor12Frames =
+    rollsFor10Frames ++ take numLastFrameBonusBalls remainingRolls
+    where
+        rollsFor10Frames = join $ take 10 rollsFor12Frames
+        remainingRolls = join $ drop 10 rollsFor12Frames
+        f10 = rollsFor12Frames !! 9
+        f10r1 = head f10
+        f10sum = sum f10
+        numLastFrameBonusBalls = if f10r1 == maxPins
+            then 2
+            else if f10sum == maxPins
+                then 1
+                else 0
+
 prop_FrameIntegrityAfterOneRoll :: [Rolls] -> Bool
-prop_FrameIntegrityAfterOneRoll rss =
+prop_FrameIntegrityAfterOneRoll rollsFor12Frames =
     runningTotal f1 == Nothing &&
     firstRoll f1 == Just r1 &&
     secondRoll f1 == Nothing &&
     thirdRoll f1 == Nothing
     where
+        rs = rollsFromRollsFor12rames rollsFor12Frames
         r1 = rs !! 0
-        rs = join rss
         frames = processRolls $ take 1 rs
         f1 = frames !! 0
 
 prop_FrameIntegrityAfterTwoRolls :: [Rolls] -> Bool
-prop_FrameIntegrityAfterTwoRolls rss =
+prop_FrameIntegrityAfterTwoRolls rollsFor12Frames =
     if r1 == maxPins then
             runningTotal f1 == Nothing &&
             firstRoll f1 == Just r1 &&
@@ -29,16 +44,16 @@ prop_FrameIntegrityAfterTwoRolls rss =
         secondRoll f1 == Just r2 &&
         thirdRoll f1 == Nothing
     where
+        rs = rollsFromRollsFor12rames rollsFor12Frames
         r1 = rs !! 0
         r2 = rs !! 1
-        rs = join rss
         frames = processRolls $ take 2 rs
         f1 = frames !! 0
 
-validFrameRolls = [ [r1, r2] | r1 <- [0..9], r2 <- [0..10], r1 + r2 <= 10] ++ [[10]]
-validFrameRollsChoice = elements validFrameRolls
-rollsFor11Frames = vectorOf 11 validFrameRollsChoice
+allValidFrameRolls = [ [r1, r2] | r1 <- [0..9], r2 <- [0..10], r1 + r2 <= 10] ++ [[10]]
+allValidFrameRollsGen = elements allValidFrameRolls
+rollsFor12FramesGen = vectorOf 12 allValidFrameRollsGen
 
 main = do
-    quickCheck (forAll rollsFor11Frames prop_FrameIntegrityAfterOneRoll)
-    quickCheck (forAll rollsFor11Frames prop_FrameIntegrityAfterTwoRolls)
+    quickCheck (forAll rollsFor12FramesGen prop_FrameIntegrityAfterOneRoll)
+    quickCheck (forAll rollsFor12FramesGen prop_FrameIntegrityAfterTwoRolls)
