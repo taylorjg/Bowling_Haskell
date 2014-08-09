b39017c71100685e6c0ed0b19a5fc26eef5b5e78
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-08-09 18:02:39 +0100

    Figured out how to pass valid list of rolls to properties

--- src/PropertyTests.hs
+++ src/PropertyTests.hs
@@ -6,33 +6,19 @@ import Bowling
 import Control.Monad (join)
 import System.Exit (exitFailure)
 
-rollsFromRollsFor12Frames :: [Rolls] -> Rolls
-rollsFromRollsFor12Frames rollsFor12Frames =
-    rollsFor10Frames ++ take numLastFrameBonusBalls remainingRolls
-    where
-        rollsFor10Frames = join $ take 10 rollsFor12Frames
-        remainingRolls = join $ drop 10 rollsFor12Frames
-        lastFrame = rollsFor12Frames !! 9
-        numLastFrameBonusBalls = calculateNumLastFrameBonusBalls lastFrame
-        calculateNumLastFrameBonusBalls [maxPins] = 2
-        calculateNumLastFrameBonusBalls [r1, r2]
-            | r1 + r2 == maxPins = 1
-            | otherwise = 0
-
-prop_FrameIntegrityAfterOneRoll :: [Rolls] -> Bool
-prop_FrameIntegrityAfterOneRoll rollsFor12Frames =
+prop_FrameIntegrityAfterOneRoll :: Rolls -> Bool
+prop_FrameIntegrityAfterOneRoll rolls =
     runningTotal f1 == Nothing &&
     firstRoll f1 == Just r1 &&
     secondRoll f1 == Nothing &&
     thirdRoll f1 == Nothing
     where
-        rs = rollsFromRollsFor12Frames rollsFor12Frames
-        r1 = rs !! 0
-        frames = processRolls $ take 1 rs
+        frames = processRolls $ take 1 rolls
         f1 = frames !! 0
+        r1 = rolls !! 0
 
-prop_FrameIntegrityAfterTwoRolls :: [Rolls] -> Bool
-prop_FrameIntegrityAfterTwoRolls rollsFor12Frames =
+prop_FrameIntegrityAfterTwoRolls :: Rolls -> Bool
+prop_FrameIntegrityAfterTwoRolls rolls =
     if r1 == maxPins then
             runningTotal f1 == Nothing &&
             firstRoll f1 == Just r1 &&
@@ -44,20 +30,36 @@ prop_FrameIntegrityAfterTwoRolls rollsFor12Frames =
         secondRoll f1 == Just r2 &&
         thirdRoll f1 == Nothing
     where
-        rs = rollsFromRollsFor12Frames rollsFor12Frames
-        r1 = rs !! 0
-        r2 = rs !! 1
-        frames = processRolls $ take 2 rs
+        frames = processRolls $ take 2 rolls
         f1 = frames !! 0
+        r1 = rolls !! 0
+        r2 = rolls !! 1
 
-allValidFrameRolls = [ [r1, r2] | r1 <- [0..9], r2 <- [0..10], r1 + r2 <= 10] ++ [[10]]
-allValidFrameRollsGen = elements allValidFrameRolls
-rollsFor12FramesGen = vectorOf 12 allValidFrameRollsGen
+-- TODO: use 'frequency' to get more strikes e.g. 50% of all pairs
+pairsOfRolls = [ [r1, r2] | r1 <- [0..9], r2 <- [0..10], r1 + r2 <= 10] ++ [[10]] :: [Rolls]
+pairsOfRollsGen = elements pairsOfRolls
 
+rollsGen :: Gen Rolls
+rollsGen = do
+    tenFrames <- vectorOf 10 pairsOfRollsGen
+    twoFrames <- vectorOf 2 pairsOfRollsGen
+    let lastFrame = last tenFrames
+    let rolls = join tenFrames
+    let bonusBalls = join twoFrames
+    let numBonusBalls = calculateNumBonusBalls lastFrame
+    return $ rolls ++ take numBonusBalls bonusBalls
+
+calculateNumBonusBalls :: Rolls -> Int
+calculateNumBonusBalls [maxPins] = 2
+calculateNumBonusBalls [r1, r2]
+    | r1 + r2 == maxPins = 1
+    | otherwise = 0
+    
 main :: IO ()
 main = do
-    r1 <- quickCheckResult (forAll rollsFor12FramesGen prop_FrameIntegrityAfterOneRoll)
-    r2 <- quickCheckResult (forAll rollsFor12FramesGen prop_FrameIntegrityAfterTwoRolls)
+    sample rollsGen
+    r1 <- quickCheckResult (forAll rollsGen prop_FrameIntegrityAfterOneRoll)
+    r2 <- quickCheckResult (forAll rollsGen prop_FrameIntegrityAfterTwoRolls)
     if all isSuccess [r1, r2]
         then return ()
         else exitFailure
